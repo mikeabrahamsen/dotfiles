c793223785ae1271a8dd61b183229448791b76cd
Mike Abrahamsen <mail@michaelabrahamsen.com>
2012-12-28 19:39:43 -0700

    changed awesome keybindings
    - changed viewprev and viewnext to be on h and l
    - changed focus inc / dec to be on t and n
    - changed swap to be on shift t and n
    - changed inc / dec master to be mod4 + mod1 + ;/q

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -205,16 +205,16 @@ root.buttons(awful.util.table.join(
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left",   awful.t***REMOVED***.viewprev       ),
-    awful.key({ modkey,           }, "Right",  awful.t***REMOVED***.viewnext       ),
+    awful.key({ modkey, "Shift"   }, "h",   awful.t***REMOVED***.viewprev       ),
+    awful.key({ modkey, "Shift"   }, "l",  awful.t***REMOVED***.viewnext       ),
     awful.key({ modkey,           }, "Escape", awful.t***REMOVED***.history.restore),
 
-    awful.key({ modkey,           }, "j",
+    awful.key({ modkey,           }, "t",
         function ()
             awful.client.focus.byidx( 1)
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey,           }, "k",
+    awful.key({ modkey,           }, "n",
         function ()
             awful.client.focus.byidx(-1)
             if client.focus then client.focus:raise() end
@@ -222,8 +222,8 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "w", function () mymainmenu:show({keygrabber=true}) end),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
+    awful.key({ modkey, "Shift"   }, "t", function () awful.client.swap.byidx(  1)    end),
+    awful.key({ modkey, "Shift"   }, "n", function () awful.client.swap.byidx( -1)    end),
     awful.key({ modkey,           }, "`", function () awful.screen.focus_relative( 1) end),
     awful.key({ modkey, "Shift"   }, "`", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
@@ -240,10 +240,10 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.t***REMOVED***.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.t***REMOVED***.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.t***REMOVED***.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.t***REMOVED***.incnmaster(-1)      end),
+    awful.key({ modkey,     }, "l",     function () awful.t***REMOVED***.incmwfact( 0.05)    end),
+    awful.key({ modkey,     }, "h",     function () awful.t***REMOVED***.incmwfact(-0.05)    end),
+    awful.key({ modkey, "Mod1"   }, "q",     function () awful.t***REMOVED***.incnmaster( 1)      end),
+    awful.key({ modkey, "Mod1"   }, ";",     function () awful.t***REMOVED***.incnmaster(-1)      end),
     awful.key({ modkey, "Control" }, "h",     function () awful.t***REMOVED***.incncol( 1)         end),
     awful.key({ modkey, "Control" }, "l",     function () awful.t***REMOVED***.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
@@ -355,11 +355,10 @@ awful.rules.rules = {
                      focus = true,
                      keys = clientkeys,
                      buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
     { rule = { class = "gimp" },
+      properties = { floating = true } },
+      
+    { rule = { class = "skype" },
       properties = { floating = true } },
     -- Set Firefox to always map on t***REMOVED***s number 2 of screen 1.
     -- { rule = { class = "Firefox" },
