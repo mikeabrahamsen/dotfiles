5eb30cf0439659de8a1ac75575096fe4d2b1ccb2
Mike Abrahamsen <mail@michaelabrahamsen.com>
2012-12-29 11:59:06 -0700

    changed tag names

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -68,10 +68,12 @@ layouts =
 
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {}
+tags = {
+    names = {"term","dev","web"}
+}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag(tags.names, s, layouts[10])
 end
 -- }}}
 
