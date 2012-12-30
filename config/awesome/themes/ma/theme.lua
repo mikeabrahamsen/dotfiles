84a46078fe3f7a126fc9cae28e93948db718709a
Mike Abrahamsen <mail@michaelabrahamsen.com>
2012-12-30 15:11:10 -0700

    updated layout and taglist icons

--- config/awesome/themes/ma/theme.lua
+++ config/awesome/themes/ma/theme.lua
@@ -33,9 +33,9 @@ theme.border_marked = "#91231c"
 
 -- Display the taglist squares
 theme.taglist_squares_sel   = "/home/ma/.config/awesome/themes/ma/taglist/squarefw.png"
-theme.taglist_squares_unsel = "/home/ma/.config/awesome/themes/ma/taglist/squarew.png"
+theme.taglist_squares_unsel = "/home/ma/.config/awesome/themes/ma/taglist/squarefw.png"
 
-theme.tasklist_floating_icon = "/home/ma/.config/awesome/themes/ma/tasklist/floatingw.png"
+theme.tasklist_floating_icon = "/home/ma/.config/awesome/themes/ma/tasklist/floating.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
@@ -77,18 +77,18 @@ theme.titlebar_maximized_button_focus_active  = "/home/ma/.config/awesome/themes
 theme.wallpaper_cmd = { "awsetbg /home/ma/.config/awesome/themes/ma/background.png" }
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/home/ma/.config/awesome/themes/ma/layouts/fairhw.png"
-theme.layout_fairv = "/home/ma/.config/awesome/themes/ma/layouts/fairvw.png"
-theme.layout_floating  = "/home/ma/.config/awesome/themes/ma/layouts/floatingw.png"
-theme.layout_magnifier = "/home/ma/.config/awesome/themes/ma/layouts/magnifierw.png"
-theme.layout_max = "/home/ma/.config/awesome/themes/ma/layouts/maxw.png"
-theme.layout_fullscreen = "/home/ma/.config/awesome/themes/ma/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/home/ma/.config/awesome/themes/ma/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/home/ma/.config/awesome/themes/ma/layouts/tileleftw.png"
-theme.layout_tile = "/home/ma/.config/awesome/themes/ma/layouts/tilew.png"
-theme.layout_tiletop = "/home/ma/.config/awesome/themes/ma/layouts/tiletopw.png"
-theme.layout_spiral  = "/home/ma/.config/awesome/themes/ma/layouts/spiralw.png"
-theme.layout_dwindle = "/home/ma/.config/awesome/themes/ma/layouts/dwindlew.png"
+theme.layout_fairh = "/home/ma/.config/awesome/themes/ma/layouts/fairh.png"
+theme.layout_fairv = "/home/ma/.config/awesome/themes/ma/layouts/fairv.png"
+theme.layout_floating  = "/home/ma/.config/awesome/themes/ma/layouts/floating.png"
+theme.layout_magnifier = "/home/ma/.config/awesome/themes/ma/layouts/magnifier.png"
+theme.layout_max = "/home/ma/.config/awesome/themes/ma/layouts/max.png"
+theme.layout_fullscreen = "/home/ma/.config/awesome/themes/ma/layouts/fullscreen.png"
+theme.layout_tilebottom = "/home/ma/.config/awesome/themes/ma/layouts/tilebottom.png"
+theme.layout_tileleft   = "/home/ma/.config/awesome/themes/ma/layouts/tileleft.png"
+theme.layout_tile = "/home/ma/.config/awesome/themes/ma/layouts/tile.png"
+theme.layout_tiletop = "/home/ma/.config/awesome/themes/ma/layouts/tiletop.png"
+theme.layout_spiral  = "/home/ma/.config/awesome/themes/ma/layouts/spiral.png"
+theme.layout_dwindle = "/home/ma/.config/awesome/themes/ma/layouts/dwindle.png"
 
 theme.awesome_icon = "/home/ma/.config/awesome/themes/ma/icons/arch-blue.png"
 
