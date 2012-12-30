2710d7ab288a4ad38814bdcfc730ab60d9fbd8c7
Mike Abrahamsen <mail@michaelabrahamsen.com>
2012-12-30 00:21:10 -0700

    major theme changes
    - added battery / cpu / memory / wifi info in status bar
    - removed conky from startup items
    - added icon library
    - created new them called ma

--- config/awesome/themes/ma/theme.lua
+++ config/awesome/themes/ma/theme.lua
@@ -32,15 +32,15 @@ theme.border_marked = "#91231c"
 --theme.t***REMOVED***list_bg_focus = "#ff0000"
 
 -- Display the t***REMOVED***list squares
-theme.t***REMOVED***list_squares_sel   = "/usr/share/awesome/themes/default/t***REMOVED***list/squarefw.png"
-theme.t***REMOVED***list_squares_unsel = "/usr/share/awesome/themes/default/t***REMOVED***list/squarew.png"
+theme.t***REMOVED***list_squares_sel   = "/home/ma/.config/awesome/themes/ma/t***REMOVED***list/squarefw.png"
+theme.t***REMOVED***list_squares_unsel = "/home/ma/.config/awesome/themes/ma/t***REMOVED***list/squarew.png"
 
-theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+theme.tasklist_floating_icon = "/home/ma/.config/awesome/themes/ma/tasklist/floatingw.png"
 
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = "/usr/share/awesome/themes/default/submenu.png"
+theme.menu_submenu_icon = "/home/ma/.config/awesome/themes/ma/submenu.png"
 theme.menu_height = "15"
 theme.menu_width  = "100"
 
@@ -50,47 +50,47 @@ theme.menu_width  = "100"
 --theme.bg_widget = "#cc0000"
 
 -- Define the im***REMOVED***e to load
-theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = "/home/ma/.config/awesome/themes/ma/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = "/home/ma/.config/awesome/themes/ma/titlebar/close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = "/home/ma/.config/awesome/themes/ma/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = "/home/ma/.config/awesome/themes/ma/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = "/home/ma/.config/awesome/themes/ma/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = "/home/ma/.config/awesome/themes/ma/titlebar/ontop_focus_active.png"
 
-theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = "/home/ma/.config/awesome/themes/ma/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = "/home/ma/.config/awesome/themes/ma/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = "/home/ma/.config/awesome/themes/ma/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = "/home/ma/.config/awesome/themes/ma/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = "/home/ma/.config/awesome/themes/ma/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = "/home/ma/.config/awesome/themes/ma/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = "/home/ma/.config/awesome/themes/ma/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = "/home/ma/.config/awesome/themes/ma/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = "/home/ma/.config/awesome/themes/ma/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = "/home/ma/.config/awesome/themes/ma/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = "/home/ma/.config/awesome/themes/ma/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = "/home/ma/.config/awesome/themes/ma/titlebar/maximized_focus_active.png"
 
 -- You can use your own command to set your wallpaper
-theme.wallpaper_cmd = { "awsetbg /usr/share/awesome/themes/default/background.png" }
+theme.wallpaper_cmd = { "awsetbg /home/ma/.config/awesome/themes/ma/background.png" }
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
-theme.layout_fairv = "/usr/share/awesome/themes/default/layouts/fairvw.png"
-theme.layout_floating  = "/usr/share/awesome/themes/default/layouts/floatingw.png"
-theme.layout_m***REMOVED***nifier = "/usr/share/awesome/themes/default/layouts/m***REMOVED***nifierw.png"
-theme.layout_max = "/usr/share/awesome/themes/default/layouts/maxw.png"
-theme.layout_fullscreen = "/usr/share/awesome/themes/default/layouts/fullscreenw.png"
-theme.layout_tilebottom = "/usr/share/awesome/themes/default/layouts/tilebottomw.png"
-theme.layout_tileleft   = "/usr/share/awesome/themes/default/layouts/tileleftw.png"
-theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
-theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
-theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
-theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+theme.layout_fairh = "/home/ma/.config/awesome/themes/ma/layouts/fairhw.png"
+theme.layout_fairv = "/home/ma/.config/awesome/themes/ma/layouts/fairvw.png"
+theme.layout_floating  = "/home/ma/.config/awesome/themes/ma/layouts/floatingw.png"
+theme.layout_m***REMOVED***nifier = "/home/ma/.config/awesome/themes/ma/layouts/m***REMOVED***nifierw.png"
+theme.layout_max = "/home/ma/.config/awesome/themes/ma/layouts/maxw.png"
+theme.layout_fullscreen = "/home/ma/.config/awesome/themes/ma/layouts/fullscreenw.png"
+theme.layout_tilebottom = "/home/ma/.config/awesome/themes/ma/layouts/tilebottomw.png"
+theme.layout_tileleft   = "/home/ma/.config/awesome/themes/ma/layouts/tileleftw.png"
+theme.layout_tile = "/home/ma/.config/awesome/themes/ma/layouts/tilew.png"
+theme.layout_tiletop = "/home/ma/.config/awesome/themes/ma/layouts/tiletopw.png"
+theme.layout_spiral  = "/home/ma/.config/awesome/themes/ma/layouts/spiralw.png"
+theme.layout_dwindle = "/home/ma/.config/awesome/themes/ma/layouts/dwindlew.png"
 
-theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+theme.awesome_icon = "/home/ma/.config/awesome/themes/ma/icons/arch-blue.png"
 
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
