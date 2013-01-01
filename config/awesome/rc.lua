2a23ee36eb6dcea2b69de377bdb8507f0944b053
Mike Abrahamsen <mail@michaelabrahamsen.com>
2013-01-01 11:49:11 -0700

    removed mem widget line that is unused

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -224,7 +224,6 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
-    right_layout:add(memwidget)
     right_layout:add(baticon)
     right_layout:add(batwidget)
     right_layout:add(mytextclock)
