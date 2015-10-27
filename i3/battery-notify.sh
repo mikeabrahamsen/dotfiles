a118d9d0d98513948fc23c57ce496261310eb6d3
Michael Abrahamsen <mail@michaelabrahamsen.com>
2015-10-27 11:53:48 -0600

    change how battery is calculated

--- i3/battery-notify.sh
+++ i3/battery-notify.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+BATTPERCENT=`cat /sys/class/power_supply/BAT1/capacity`
+BATTSTATUS=`cat /sys/class/power_supply/BAT1/status`
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:20:00 ]] ; then
+
+if [[ `echo $BATTSTATUS | grep Discharging` && `echo $BATTPERCENT` -lt 20 ]] ; then
     DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
 fi
