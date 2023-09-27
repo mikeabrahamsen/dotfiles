984fb11f57dd47d85d8ab78a24dc4bb69ab27ed2
Michael Abrahamsen <mail@michaelabrahamsen.com>
2023-09-27 15:45:12 -0600

    add quickfix toggle

--- config/nvim/lua/ma/remap.lua
+++ config/nvim/lua/ma/remap.lua
@@ -5,3 +5,19 @@ vim.keymap.set("n", "<c-j>", "<c-w>j")
 vim.keymap.set("n", "<C-k>", "<c-w>k")
 vim.keymap.set("n", "<c-l>", "<c-w>l")
 vim.keymap.set("n", "<c-h>", "<c-w>h")
+vim.keymap.set("n", "<leader>q", [[:lua ToggleQuickfix()<CR>]])
+
+function ToggleQuickfix()
+  local qf_exists = false
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win["quickfix"] == 1 then
+      qf_exists = true
+    end
+  end
+  if qf_exists == true then
+        vim.cmd('cclose')
+    else
+        print("Quickfix window open")
+        vim.cmd('botright copen')
+    end
+end
