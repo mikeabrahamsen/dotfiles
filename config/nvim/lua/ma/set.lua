07a17659ff7f9269d96ee62b34f93f7cc366bb5a
Michael Abrahamsen <mail@michaelabrahamsen.com>
2023-09-30 15:16:18 -0600

    add persistent undo

--- config/nvim/lua/ma/set.lua
+++ config/nvim/lua/ma/set.lua
@@ -18,6 +18,13 @@ vim.opt.backup = true
 vim.opt.backupdir = os.getenv( "HOME" ) .. '/tmp/vim/backup'
 vim.opt.directory = os.getenv( "HOME" ) .. '/tmp/vim/swap'
 
+-- Check for persistent undo support
+if vim.fn.has("persistent_undo") == 1 then
+  vim.opt.undofile = true
+  local home_dir = vim.fn.getenv("HOME")
+  vim.opt.undodir = home_dir .. '/tmp/vim/undo'
+end
+
 vim.opt.wrap = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
