c0eaa247816ad2a1e107aa579877ee4dfd8003e5
Michael Abrahamsen <mail@michaelabrahamsen.com>
2023-09-30 15:15:08 -0600

    telescope config

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,16 +1,12 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader><C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set("n", "<leader><C-b>", builtin.buffers, {})
+vim.keymap.set('n', '<leader><C-p>', builtin.find_files, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+vim.keymap.set("n", "<leader>:", builtin.command_history, {})
+vim.keymap.set("n", "<leader>/", builtin.search_history, {})
+vim.keymap.set("n", "<leader>man", builtin.man_pages, {})
+
+
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-
-vim.keymap.set('n', '<leader>PS', function()
-    local selected_text = vim.fn.getreg('v')
-    if selected_text ~= "" then
-        builtin.grep_string({ search = selected_text })
-    else
-        print("No text selected in visual mode.")
-    end
 end)
