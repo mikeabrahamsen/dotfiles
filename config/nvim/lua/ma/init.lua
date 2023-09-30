3c1af8e1a7678edf7f003592707bd44d395175bf
Michael Abrahamsen <mail@michaelabrahamsen.com>
2023-09-29 19:15:43 -0600

    remove trailing whitespace on save

--- config/nvim/lua/ma/init.lua
+++ config/nvim/lua/ma/init.lua
@@ -6,3 +6,11 @@ require("mason").setup()
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+-- Trim trailing whitespace before saving
+vim.cmd([[
+  augroup TrimWhitespace
+    autocmd!
+    autocmd BufWritePre * :%s/\s\+$//e
+  augroup END
+]])
