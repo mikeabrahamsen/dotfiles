0d9df7d391767cbc51473fe3e51fb6b537beafad
Michael Abrahamsen <mail@michaelabrahamsen.com>
2025-06-23 11:19:41 -0600

    add ccode plugin

--- config/nvim/lua/ma/init.lua
+++ config/nvim/lua/ma/init.lua
@@ -6,6 +6,7 @@ require("mason").setup()
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.cmd[[ClaudeCode]]
 
 -- Trim trailing whitespace before saving
 vim.cmd([[
